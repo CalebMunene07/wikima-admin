14a5e47844d8dbb1e4953ebbfa4f3a5103dd437d
CalebMunene07 <munenecaleb007@gmail.com>
2026-05-22 13:06:54 +0300

    changed app/page.tsx and eslint.config.mjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 export const runtime = 'edge';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.redirect(new URL('/login', 'https://wikima-admin.pages.dev'));
-}
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return null;
+  redirect('/login');
 }
